4f3052ad147d274fd3936f7f965b198410502406
kaitabuchi <kaidanieltabuchi@gmail.com>
2024-07-18 09:30:11 -0500

    More textures!

--- SquareEngine/SquareEngine/src/Renderer.cpp
+++ SquareEngine/SquareEngine/src/Renderer.cpp
@@ -28,9 +28,9 @@ namespace Square
 	void Renderer::RenderMesh(Mesh* mesh, float shine, Texture2D texture, glm::vec3 position, glm::vec3 rotation, glm::vec3 scale)
 	{
 		glm::mat4 model = glm::translate(glm::mat4(1.0f), position);
-		model = glm::rotate(model, rotation.x, glm::vec3(1, 0, 0));
-		model = glm::rotate(model, rotation.y, glm::vec3(0, 1, 0));
-		model = glm::rotate(model, rotation.z, glm::vec3(0, 0, 1));
+		model = glm::rotate(model, glm::radians(rotation.x), glm::vec3(1, 0, 0));
+		model = glm::rotate(model, glm::radians(rotation.y), glm::vec3(0, 1, 0));
+		model = glm::rotate(model, glm::radians(rotation.z), glm::vec3(0, 0, 1));
 		model = glm::scale(model, scale);
 
 		program.SetMaterialProperties(GetMainCamera()->position, shine);
