445ce987d14a20a654371f3b0ef285bfde2d3cef
kaitabuchi <kaidanieltabuchi@gmail.com>
2024-11-26 12:40:27 -0600

    remove problem nuclearly

--- SquareEngine/Projects/Test/premake5-managed.lua
+++
@@ -1,9 +0,0 @@
-workspace "Test"
-    configurations { "Debug", "Release" }
-
-    targetdir "Binaries"
-
--- this needs to be relative to the coral dir instead of ../
-include "../../Coral.Managed/premake5.lua"
-
-include "premake5.lua"
